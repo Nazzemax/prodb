fa28a515afb8cb8462eb52aa79f45cd003911a19
Nazzemax <hvan1998@inbox.ru>
2025-03-20 01:06:04 +0600

    fix:dfddffd

--- src/components/atoms/floating-whatsapp/index.tsx
+++ src/components/atoms/floating-whatsapp/index.tsx
@@ -1,19 +1,35 @@
 'use client';
 
-import React from 'react'
+import { useAppData } from '@/context/app-context';
+import React, { useEffect, useState } from 'react'
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 const FloatingWhatsapp = () => {
-    return <FloatingWhatsApp
-        phoneNumber="+996999504444" // Номер телефона в международном формате
-        accountName="Bold Brands International"
-        notificationSound
-        chatMessage="Доброго времени суток, чем могу вам помочь?"
-        statusMessage="Онлайн"
-        darkMode
-        avatar="https://bishkek.headhunter.kg/employer-logo/6266415.png"
-        placeholder="Введите текст"
-    />
+    const [mounted, setMounted] = useState(false)
+    const isUz = localStorage.getItem("locale")
+    const { data } = useAppData()
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null;
+    }
+
+    if(!data) return null
+
+    return (
+        <FloatingWhatsApp
+            phoneNumber={isUz === "uz" ? data?.phones[1].phone.split(" ").join("") : data?.phones[0].phone.split(" ").join("") } 
+            accountName="Bold Brands International"
+            notificationSound
+            chatMessage="Доброго времени суток, чем могу вам помочь?"
+            statusMessage="Онлайн"
+            darkMode
+            avatar="https://bishkek.headhunter.kg/employer-logo/6266415.png"
+            placeholder="Введите текст"
+        />
+    );
 }
 
 export default FloatingWhatsapp;
