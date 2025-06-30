22f26fc9bbffe1523195143df87230e178b936f2
Nazzemax <hvan1998@inbox.ru>
2025-07-01 01:14:35 +0600

    fix final

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -70,6 +70,15 @@ export default async function LocaleLayout({ children, params }: Props) {
     return (
         <html lang={locale}>
             <head>
+                <Script id="gtag-script" strategy="lazyOnload"
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TWLSLSMG');`
+                    }}
+                />
                 <Script id="yandex-metrika-script" strategy="lazyOnload">
                     {`
     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
