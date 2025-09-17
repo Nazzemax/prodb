098101289d44b922a6262269d5cab06c31b41278
Nazzemax <hvan1998@inbox.ru>
2025-09-17 20:33:53 +0600

    fix removed extra things added metric gogle

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -99,9 +99,25 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     });
   `}
                 </Script>
-             
+                <Script id="fb-pixel-script" strategy="lazyOnload">
+                    {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '681332601016811');
+          fbq('track', 'PageView');
+        `}
+                </Script>
+              
+              
                 <meta name="viewport" content="initial-scale=1, width=device-width" />
 
+             
                 <noscript>
                     <div>
                         <img
@@ -139,6 +155,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 </noscript>
             </head>
             <body className={`${cannonade.className} antialiased `}>
+                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TWLSLSMG"
+                    height="0" width="0" style={{display:'none', visibility:'hidden'}}></iframe></noscript>
                 <noscript>
                     <div>
                         {}
