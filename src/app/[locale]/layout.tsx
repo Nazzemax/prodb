c9923f85a723e80c1acad11224e2d86ca55f6346
Nazzemax <hvan1998@inbox.ru>
2025-09-17 20:25:51 +0600

    fix:removed extra metrics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -99,47 +99,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     });
   `}
                 </Script>
-                <Script id="fb-pixel-script" strategy="lazyOnload">
-                    {`  
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '681332601016811');
-          fbq('track', 'PageView');
-        `}
-                </Script>
-                <Script
-                    src="https://www.googletagmanager.com/gtag/js?id=G-JFDR5HL958"
-                    strategy="lazyOnload"
-                    async
-                />
-                <Script id="gtag-inline-script" strategy="lazyOnload">
-                    {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-JFDR5HL958');
-          `}
-                </Script>
+             
                 <meta name="viewport" content="initial-scale=1, width=device-width" />
 
-                <Script
-                    id="yandex-metrika"
-                    strategy="lazyOnload"
-                    dangerouslySetInnerHTML={{
-                        __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j=0;j<document.scripts.length;j++){if (document.scripts[j].src===r){return;}}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-            ym(100644188, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true });`,
-                    }}
-                />
                 <noscript>
                     <div>
                         <img
