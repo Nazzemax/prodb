5400795d74c66346e6f922167960de88b49fdc1a
Nazzemax <hvan1998@inbox.ru>
2025-09-17 17:22:54 +0600

    fixed issue with google tag script

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -70,13 +70,15 @@ export default async function LocaleLayout({ children, params }: Props) {
     return (
         <html lang={locale}>
             <head>
-                <Script id="gtag-script" strategy="lazyOnload"
+                <Script
+                    id="gtag-script"
+                    strategy="lazyOnload"
                     dangerouslySetInnerHTML={{
                         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TWLSLSMG');`
+})(window,document,'script','dataLayer','GTM-TWLSLSMG');`,
                     }}
                 />
                 <Script id="yandex-metrika-script" strategy="lazyOnload">
