350ca9cf16db12eefa2f9b2a38f2bb06d1ec569e
Nazzemax <hvan1998@inbox.ru>
2025-10-02 21:46:46 +0600

    fix contact page added there form

--- src/app/[locale]/contacts/page.tsx
+++ src/app/[locale]/contacts/page.tsx
@@ -1,20 +1,22 @@
-import FeedbackForm from "@/components/forms/feedback-form";
 import FormLayout from "@/components/templates/form-layout";
 import { Map } from "./Map";
 import { Metadata } from "next";
+import { CostCalculationForm } from "@/components/forms/cost-calculation-form";
+import { getPromotionTypes } from "@/api/Types";
 
 export const metadata: Metadata = {
     title: "Контакты",
     description: "Получите бесплатную консультацию в Bold Brands International"
 };
 
-const ContactsPage = () => {
+const ContactsPage = async () => {
+    const promotion_types = await getPromotionTypes();
     return (
         <>
             <FormLayout
                 isContactPage
                 title={'Получите бесплатную консультацию'}
-                nestedForm={<FeedbackForm />}
+                nestedForm={<CostCalculationForm promotion_types={promotion_types} />}
             />
             <Map/>
         </>
